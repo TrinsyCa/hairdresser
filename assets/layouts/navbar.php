1095a959cfc2fe40780629232076252b0c5dff10
Ömer İslamoglu <omer@trinsyca.com>
2025-03-03 21:36:18 +0300

    navbar reservation link update

--- assets/layouts/navbar.php
+++ assets/layouts/navbar.php
@@ -13,14 +13,12 @@
         <div class="url">
             <a onclick="menuBars('close');" href="/#"><span>Anasayfa</span></a>
             <a onclick="menuBars('close');" href="/#hizmetler"><span>Hizmetler</span></a>
-            <a onclick="menuBars('close');" target="_blank" href="https://www.kolayrandevu.com/randevu3.php?hizmetler%5B%5D=28-1&kampanya=&kampanya_musteri=&referans=&sube=175867&website=1"><span>Randevu Al</span></a>
+            <a onclick="menuBars('close');" target="_blank" href="https://www.kolayrandevu.com/isletme/seval-sirakaya-guzellik-ve-sac?website=1"><span>Randevu Al</span></a>
             <a onclick="menuBars('close');" href="/#iletisim"><span>İletişim</span></a>
             <a onclick="menuBars('close');" href="/#hakkimizda"><span>Hakkımızda</span></a>
         </div>
         <div class="social-media">
             <a aria-label="Instagram'da Seval Sırakaya Güzellik ve Saç Salonu'nu ziyaret et" href="https://instagram.com/sevalsirakayaguzellik" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg></a>
-            <a aria-label="Facebook'ta Seval Sırakaya Güzellik ve Saç Salonu'nu ziyaret et" href="https://facebook.com/sevalsarikayaguzelliksalonu/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"/></svg></a>
-            <a aria-label="X'te Seval Sırakaya Güzellik ve Saç Salonu'nu ziyaret et" href="https://x.com/sevalsarikayaguzelliksalonu/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m17.687 3.063l-4.996 5.711l-4.32-5.711H2.112l7.477 9.776l-7.086 8.099h3.034l5.469-6.25l4.78 6.25h6.102l-7.794-10.304l6.625-7.571zm-1.064 16.06L5.654 4.782h1.803l10.846 14.34z"/></svg></a>
         </div>
     </div>
 </nav>
